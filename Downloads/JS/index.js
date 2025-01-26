2a947b10e2bc680d4111665ddb2983dcaa5f9f89
lucypetry <martina.falchi11@gmail.com>
2025-01-26 16:51:12 +0100

    esercizi js

---
+++ Downloads/JS/index.js
@@ -0,0 +1,4 @@
+console.log(document.querySelector("ul").parentNode);
+console.log(document.querySelector("ul").children[1]);
+console.log(document.querySelector("ul").children[1].nextElementSibling);
+console.log(document.querySelector("ul").children[1].previousElementSibling);
